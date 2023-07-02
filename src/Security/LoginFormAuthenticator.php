a75235958f0b160cafda6c0486ee71623cfa743c
unknown <seck_djiby@yahoo.fr>
2023-07-02 22:12:07 +0200

    MCD

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -47,7 +47,7 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('app_admin_accueil'));
+        return new RedirectResponse($this->urlGenerator->generate('app_profil'));
         //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
