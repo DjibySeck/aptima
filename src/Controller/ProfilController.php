08c94a0506cb450f883f7167adcb4b4e7566cd4d
unknown <seck_djiby@yahoo.fr>
2023-07-04 07:19:04 +0200

    inversedBy a la main

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -6,6 +6,30 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Entity\User;
+use App\Entity\StatutAgent;
+use App\Entity\Statut;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+
+class ProfilController extends AbstractController
+{
+    #[Route('/profil', name: 'app_profil')]
+    public function index(UserRepository $userRepository): Response
+    {
+        return $this->render('profil/index.html.twig', [
+            'users' => $userRepository->findAll(),
+        ]);
+    }
+}
+
+/*
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 class ProfilController extends AbstractController
 {
     #[Route('/profil', name: 'app_profil')]
@@ -16,3 +40,4 @@ class ProfilController extends AbstractController
         ]);
     }
 }
+*/
